6a9b42b825eaa41dababa79f92b4530a2e9faf64
KGH <knm128882@gmail.com>
2023-03-03 09:55:43 +0900

    플레이어 이동(Git 230303 김규환 설명 확인)

--- KpmvUnity/Assets/Scripts/PlayerMotion.cs
+++ KpmvUnity/Assets/Scripts/PlayerMotion.cs
@@ -95,7 +95,7 @@ public class PlayerMotion : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Floor"))
+        if (collision.gameObject.CompareTag("Floor")) //바닥 태그 추가
         {
             isGrounded = true;
         }
